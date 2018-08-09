205b801885b861bc0c44b9cc6dc0dc67c73d4e6c
Nadeem Baig <nadeembaig@Nadeems-MacBook-Pro.local>
2018-08-09 16:12:51 -0500

    setter injection

---
+++ src/com/ngnteks/springdemo/CricketCoach.java
@@ -0,0 +1,31 @@
+package com.ngnteks.springdemo;
+
+public class CricketCoach implements Coach {
+
+	private FortuneService fortuneService;
+	private String emailAddress;
+	private String teamName;
+	
+	//create a no-args constructor
+	public CricketCoach() {
+		System.out.println("CricketCoach- inside no-args constructor");
+	}
+	//our setter method
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("CricketCoach- inside setter method - setFortuneService");
+		this.fortuneService = fortuneService;
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Practice fast bowling for 15 minutes a day!";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		
+		return fortuneService.getFortune();
+	}
+
+}
---
+++ src/com/ngnteks/springdemo/SetterDemoApp.java
@@ -0,0 +1,22 @@
+package com.ngnteks.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SetterDemoApp {
+
+	public static void main(String[] args) {
+		//load the spring configuration file
+		
+		ClassPathXmlApplicationContext context =
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		//retreive bean from spring container
+		
+		CricketCoach myCoach = context.getBean("myCricketCoach", CricketCoach.class);
+		//call methods on bean
+		System.out.println(myCoach.getDailyWorkout());
+		System.out.println(myCoach.getDailyFortune());
+		//close context
+		context.close();
+	}
+
+}
